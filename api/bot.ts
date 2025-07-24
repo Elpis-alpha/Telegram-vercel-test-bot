233cfb9912f649411a644be9d5b030fe394bc7e1
Elpis <elpis409@gmail.com>
2025-07-24 16:41:22 +0100

    with ip

--- api/bot.ts
+++ api/bot.ts
@@ -26,6 +26,7 @@ bot.launch(
   {
     webhook: {
       domain: "http://bot-de.duckdns.org", // Replace with your Vercel app URL
+      ipAddress: "http://217.196.51.52:5013/",
       port: 5000,
     },
   },
