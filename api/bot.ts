1b47498c00b25c0bec00ac7d55d6d9d06cb13fd6
Elpis <elpis409@gmail.com>
2025-07-26 15:18:27 +0100

    end

--- api/bot.ts
+++ api/bot.ts
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
 import { Telegraf } from "telegraf";
-// import { VercelRequest, VercelResponse } from "@vercel/node";
 
 dotenv.config();
 
-// Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
-// Handle the /start command
 bot.start((ctx) => {
   ctx.reply("Hello!");
 });
@@ -17,10 +14,23 @@ bot.command("process", async (ctx) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     ctx.reply(`Processing... ${i + 1}/10`);
   }
-
+  
   await ctx.reply("Processing complete!");
 });
 
+bot.launch(
+  {
+    webhook: {
+      domain: "https://bot.dedash.fun",
+      port: 5000,
+    },
+  },
+  () => {
+    console.log("Bot is running and listening for updates...");
+  }
+);
+
+// import { VercelRequest, VercelResponse } from "@vercel/node";
 // // Webhook handler function for Vercel
 // export default async function handler(req: VercelRequest, res: VercelResponse) {
 //   try {
@@ -30,15 +40,3 @@ bot.command("process", async (ctx) => {
 //     res.status(500).send("Internal Server Error");
 //   }
 // }
-
-bot.launch(
-  {
-    webhook: {
-      domain: "https://bot.dedash.fun", // Replace with your Vercel app URL
-      port: 5000,
-    },
-  },
-  () => {
-    console.log("Bot is running and listening for updates...");
-  }
-);
